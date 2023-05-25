d0d6e7715117797771f685a081fd6b9c36a7afda
Daniel Davidson <danielmcraedavidson@gmail.com>
2023-05-24 20:09:32 -0500

    created pool

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const users = require('./data/users.js');
-
+const pg = require('pg');
+const DB_PASSWORD = process.env.DB_PASSWORD;
 const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -9,8 +10,28 @@ app.use(express.static('server/public'));
 
 app.use(express.json());
 
+const pool = new pg.Pool({
+    host: 'db.bit.io',
+    port: 5432,
+    ssl: true,
+    database: 'steveshumaker/trial',
+    user: 'songs',
+    password: DB_PASSWORD,
+})
+
 app.get('/users', (req, res) => {
    res.status(200).send(users); 
+});
+
+app.get('/messages', (req, res) => {
+    let queryText = 'SELECT * FROM messages;';
+    pool.query(queryText)
+        .then((result) => {
+            res.status(200).send(result.rows);
+        }).catch((error) => {
+            console.error(error);
+            res.sendStatus(500);
+        });
 });
 
 app.listen(PORT, () => {
