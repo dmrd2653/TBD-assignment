b15631671ec4515944f79a2ac73f28c0d4242120
Daniel Davidson <danielmcraedavidson@gmail.com>
2023-05-24 21:02:43 -0500

    started on form, need to finish POST and GET

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,7 @@
 console.log('linked');
 
-
+// fetch GET
+// turn into a function
 fetch('/messages')
 .then((response) => {
     // console.log('Success', response);
@@ -17,3 +18,34 @@ fetch('/messages')
 .catch((error) => {
     console.log('Error', error);
 });
+
+// fetch POST
+function sendMathProb(event) {
+    event.preventDefault();
+
+    let title = document.querySelector('#title').value;
+    let text = document.querySelector('#text').value;
+    let time = document.querySelector('#time_sent').value;
+    newMessage = {
+        title,
+        text,
+        time
+    };
+    // POST request to send object to server
+    fetch('/messageList', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newMessage),
+    }).then((response) => {
+        console.log('POST Response:', response);
+        // Clear the form
+        document.querySelector('#title').value = '';
+        document.querySelector('#text').value = '';
+        document.querySelector('#time_sent').value = '';
+        getEquation();
+    }).catch((error) => {
+        console.log('OH No......', error);
+        alert('Something went wrong.');
+    });
+
+};
