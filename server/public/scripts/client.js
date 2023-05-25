32769706d0035a0b5310ff831b5b195faeff9208
Daniel Davidson <danielmcraedavidson@gmail.com>
2023-05-24 21:06:02 -0500

    commented out POST, unfinished

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -19,33 +19,33 @@ fetch('/messages')
     console.log('Error', error);
 });
 
-// fetch POST
-function sendMathProb(event) {
-    event.preventDefault();
+// // fetch POST
+// function sendMathProb(event) {
+//     event.preventDefault();
 
-    let title = document.querySelector('#title').value;
-    let text = document.querySelector('#text').value;
-    let time = document.querySelector('#time_sent').value;
-    newMessage = {
-        title,
-        text,
-        time
-    };
-    // POST request to send object to server
-    fetch('/messageList', {
-        method: 'POST',
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(newMessage),
-    }).then((response) => {
-        console.log('POST Response:', response);
-        // Clear the form
-        document.querySelector('#title').value = '';
-        document.querySelector('#text').value = '';
-        document.querySelector('#time_sent').value = '';
-        getEquation();
-    }).catch((error) => {
-        console.log('OH No......', error);
-        alert('Something went wrong.');
-    });
+//     let title = document.querySelector('#title').value;
+//     let text = document.querySelector('#text').value;
+//     let time = document.querySelector('#time_sent').value;
+//     newMessage = {
+//         title,
+//         text,
+//         time
+//     };
+//     // POST request to send object to server
+//     fetch('/messageList', {
+//         method: 'POST',
+//         headers: {"Content-Type": "application/json"},
+//         body: JSON.stringify(newMessage),
+//     }).then((response) => {
+//         console.log('POST Response:', response);
+//         // Clear the form
+//         document.querySelector('#title').value = '';
+//         document.querySelector('#text').value = '';
+//         document.querySelector('#time_sent').value = '';
+//         getEquation();
+//     }).catch((error) => {
+//         console.log('OH No......', error);
+//         alert('Something went wrong.');
+//     });
 
-};
+// };
