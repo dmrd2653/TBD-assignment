818ab3a13946a4023c3dac442b88de09780d46ee
Daniel Davidson <danielmcraedavidson@gmail.com>
2023-05-24 19:30:37 -0500

    added GET request

---
+++ server/data/users.js
@@ -0,0 +1,15 @@
+let users = [
+    {
+        name: 'Steve',
+        city: 'Nashville',
+        state: 'TN'
+    },
+
+    {
+        name: 'Daniel',
+        city: 'St. Paul',
+        state: 'MN'
+    }
+];
+
+module.exports = users;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const users = require('./data/users.js');
 
 const PORT = 8000;
 
@@ -7,6 +8,10 @@ const app = express();
 app.use(express.static('server/public'));
 
 app.use(express.json());
+
+app.get('/users', (req, res) => {
+   res.status(200).send(users); 
+});
 
 app.listen(PORT, () => {
     console.log('App is listening on port', PORT);
